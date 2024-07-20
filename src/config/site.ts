9d0496fcaacc05ce4f15e41f55ceba860338f0f3
rcsen <rohansen856@gmail.com>
2024-07-20 17:28:41 +0530

    typo fixes

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,8 +4,8 @@ export const siteConfig: SiteConfig = {
   name: "ctrl-crew",
   description:
     "An open source application built using the new router, server components and everything new in Next.js 13.",
-  url: "https://ctrl-cre.vercel.app",
-  ogImage: "https://ctrl-cre.vercel.app/og.jpg",
+  url: "https://ctrl-crew.vercel.app",
+  ogImage: "https://ctrl-crew.vercel.app/og.jpg",
   links: {
     twitter: "https://twitter.com/ctrl-crew",
     github: "https://github.com/rohansen856",
